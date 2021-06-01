f55251a93774ea4f42ad3be391f026fafaa05d40
Johndhee <Johndheeballesteros13@gmail.com>
2021-06-01 16:10:35 -0700

    migration data

--- backend/db/migrate/20210601225013_create_competition.rb
+++ backend/db/migrate/20210601225013_create_competition.rb
@@ -1,4 +1,9 @@
 class CreateCompetition < ActiveRecord::Migration[5.2]
   def change
+    create_table :competitions do |t|
+      t.string :name
+      t.integer :date
+      t.integer :prize
+    end
   end
 end
--- backend/db/migrate/20210601225024_create_game.rb
+++ backend/db/migrate/20210601225024_create_game.rb
@@ -1,4 +1,9 @@
 class CreateGame < ActiveRecord::Migration[5.2]
   def change
+    create_table :games do |t|
+      t.integer :player_id
+      t.integer :competition_id
+      t.string :name
+    end
   end
 end
